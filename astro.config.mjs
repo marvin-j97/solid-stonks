91d494146a543dd62289223ccdfddc98587de565
marvin-j97 <marvin.janke.97@gmail.com>
2023-07-14 23:28:51 +0200

    fix base path for real

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marvin-j97.github.io',
-  base: '/solid-stonks',
+  base: '/solid-stonks/gh-pages',
   integrations: [solidJs(), tailwind()]
 });
