ad7db296cbec57cd9089004a78c6ccf61c734f96
marvin-j97 <marvin.janke.97@gmail.com>
2023-07-22 20:36:09 +0200

    add lag radar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { allFakers } from "@faker-js/faker";
-import { createSignal, onMount } from "solid-js";
+import { JSX, createEffect, createSignal, onCleanup } from "solid-js";
+import lagRadar from "@gaearon/lag-radar";
 
 import SolidStockTable from "./SolidStockTable";
 
-function Market(props: any) {
+const DATA_SET_SIZE = 1000;
+
+function Market(props: any): JSX.Element {
   const [market, setMarket] = createSignal(
-    Array.from({ length: 1000 }, () => ({
+    Array.from({ length: DATA_SET_SIZE }, () => ({
       name: props.faker.company.name(),
       symbol: props.faker.finance.currencyCode(),
       price: Math.floor(Math.random() * 1000),
@@ -13,9 +16,11 @@ function Market(props: any) {
     }))
   );
 
-  onMount(() => {
-    const interval = setInterval(() => {
-      for (const stock of market()) {
+  const interval = setInterval(() => {
+    setMarket((prev) => {
+      const copy = structuredClone(prev);
+
+      for (const stock of copy) {
         const chance = Math.random();
         const delta = Math.floor(Math.random() * 100);
         if (chance < 0.1) {
@@ -26,22 +31,36 @@ function Market(props: any) {
           stock.delta = delta;
         }
       }
+      copy.sort((a, b) => b.price - a.price);
+      return copy;
+    });
+  }, 1_000);
 
-      market().sort((a, b) => b.price - a.price);
-      setMarket((prev) => prev.slice().sort((a, b) => b.price - a.price));
-    }, 1000);
-
-    return () => clearInterval(interval);
-  });
+  onCleanup(() => clearInterval(interval));
 
   return <SolidStockTable data={market()} />;
 }
 
-function App() {
+function App(): JSX.Element {
+  createEffect(() => {
+    const destroyLagRadar = lagRadar({
+      frames: 50, // number of frames to draw, more = worse performance
+      speed: 0.0017, // how fast the sweep moves (rads per ms)
+      size: 100, // outer frame px
+      inset: 3, // circle inset px
+      parent: document.getElementById("radar"), // DOM node to attach to
+    });
+
+    return destroyLagRadar;
+  });
+
   return (
-    <div class="p-3">
-      <div class="loader" />
-      <p>Currently Using: Solid {"<3"}</p>
+    <div class="p-3 flex flex-col gap-2">
+      <div id="radar" />
+      <p>
+        Currently Using:{" "}
+        <span class="text-blue-500 font-bold">{"Solid <3"}</span>
+      </p>
       <div class="flex">
         <Market faker={allFakers["en"]} />
         <Market faker={allFakers["es_MX"]} />
--- src/SolidStockTable.tsx
+++ src/SolidStockTable.tsx
@@ -1,6 +1,15 @@
-import { For } from "solid-js";
+import { For, JSX } from "solid-js";
 
-export default function StockTable(props: { data: any[] }) {
+type Props = {
+  data: {
+    name: string;
+    symbol: string;
+    price: number;
+    delta: number;
+  }[];
+};
+
+export default function StockTable(props: Props): JSX.Element {
   return (
     <table class="border border-gray-200">
       <thead>
@@ -14,31 +23,22 @@ export default function StockTable(props: { data: any[] }) {
         <For each={props.data}>
           {(item) => (
             <tr>
-              <td class="px-3 py-0 border-b border-gray-200 bg-white text-xs">
+              <td class="px-3 py-1 border-b border-gray-200 bg-white text-xs">
                 <p>
                   {item.name} ({item.symbol}) is at ${item.price}
                 </p>
                 <div
                   style={{ width: `${Math.abs(item.delta)}px`, height: "10px" }}
-                  class={item.delta >= 0 ? "bg-green-500" : "bg-red-500"}
-                ></div>
+                  classList={{
+                    rounded: true,
+                    "bg-emerald-500": item.delta >= 0,
+                    "bg-red-500": item.delta < 0,
+                  }}
+                />
               </td>
             </tr>
           )}
         </For>
-        {/* {data.map(({ name, symbol, price, delta }) => (
-          <tr key={name + symbol}>
-            <td class="px-3 py-0 border-b border-gray-200 bg-white text-xs">
-              <p>
-                {name} ({symbol}) is at ${price}
-              </p>
-              <div
-                style={{ width: `${Math.abs(delta)}px`, height: '10px' }}
-                class={delta >= 0 ? 'bg-green-500' : 'bg-red-500'}
-              ></div>
-            </td>
-          </tr>
-        ))} */}
       </tbody>
     </table>
   );
